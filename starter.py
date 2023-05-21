387a020ca2b871f516a2ab7cab8f6b1e919e9cf5
Ohwowreally <FKlamidi@gmail.com>
2023-05-21 15:58:48 +0100

    datatypes
    added on 21/05/2023

---
+++ starter.py
@@ -0,0 +1,36 @@
+#name = ('Fareedah')
+#print (name)
+
+#a=6
+#b=5
+#print(a+b)
+
+#c='6'
+#d='5'
+#print(c+d)
+#---------------------------------------------------------------------------------
+z=8
+n=4
+print(z+n)
+
+r=9.7
+p=9.8
+print(r+p)
+
+print('hello')
+
+print('r','e','d')
+
+pens=('black','blue','green','red')
+print(pens)
+
+student={'name':'lola',
+          'age':'12'}
+print(student)
+
+f=False
+t=True
+print(f)
+
+student_code_names={123,456,789}
+print(student_code_names)
